08efc82838fed58e784b1d27f980de8ddb3aabb7
Eli Cusic <tntitans31@gmail.com>
2021-04-05 17:03:18 -0500

    altered url to fetch backend off heroku

--- src/apiRoot.js
+++ src/apiRoot.js
@@ -1,3 +1,3 @@
 const DEV_URL = "http://localhost:3001/"
-const PROD_URL = "http://etcusic.github.io/my-sous-backend"
+const PROD_URL = "https://my-sous-backend.herokuapp.com/"
 export const API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL
